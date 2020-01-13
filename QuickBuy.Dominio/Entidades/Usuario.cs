9ad0887a5d5cbb49fcdecd393605504fe8107c5b
João Garcia Neto <jgneto2010@hotmail.com>
2020-01-13 13:17:46 -0300

    novas implementaçoes entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string  NomeUsuario { get; set; }
+        public string SobreNome { get; set; }
+        public ICollection<Pedido> Pedidos { get; set; }
+
+    }
+}
